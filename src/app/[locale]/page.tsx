5c0374addd7ab465afeb2aad874cc161f3eb9c1c
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-08-01 11:45:50 +0300

    v0.4

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
           label: "Découvrir nos services",
           href: "/services",
         }}
-        backgroundImage="https://res.cloudinary.com/dikefxjpd/image/upload/v1754026495/bannie%C3%A8re_2_se0bvh.jpg"
+        backgroundImage="/images/back.jpg"
         secondaryButton={{
           label: "Voir la démo",
           href: "/demo",
